e0173edb8253adb88009423c0f0694abbe122ed9
Rafael Farias Marinheiro <rfm3@imac01.local>
2013-04-19 18:10:58 -0300

    Documentando o que falta ser implementado na classe rasterizer. Uma boa refatoração será eventualmente necessária.

--- Render/Rasterizer.h
+++ Render/Rasterizer.h
@@ -37,17 +37,27 @@ namespace drawing{
             }
         }
         
+        //FALTA IMPLEMENTAR
         math::Vector getBaricentricCoordinates(geometry::Point2D p, geometry::Point2D a, geometry::Point2D b, geometry::Point2D c);
         
         void resetZBuffer();
+        
+        //FALTA IMPLEMENTAR
         bool collinear(geometry::Point2D a, geometry::Point2D b, geometry::Point2D c);
+        
+        //FALTA IMPLEMENTAR
         geometry::Point3D interpolate(math::Vector baricentric, geometry::Point3D a, geometry::Point3D b, geometry::Point3D c);
+        
+        //FALTA IMPLEMENTAR
         geometry::Vector3D interpolate(math::Vector baricentric, geometry::Vector3D a, geometry::Vector3D b, geometry::Vector3D c);
         
+        //FALTA IMPLEMENTAR
         rendering::ColorVector calculateDiffuse(rendering::World * world, rendering::Camera * camera, const data::Object & object, const geometry::Point3D & position, const geometry::Vector3D normal);
         
+        //FALTA IMPLEMENTAR
         rendering::ColorVector calculateSpecular(rendering::World * world, rendering::Camera * camera, const data::Object & object, const geometry::Point3D & position, const geometry::Vector3D normal);
         
+        //FALTA IMPLEMENTAR
         rendering::ColorVector calculateAmbient(rendering::World * world);
         
     public:
